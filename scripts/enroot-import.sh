e09b8521be70cd3f5f3464548027edfa108ca641
zxoxo45@uni-tuebingen.de <tu_zxoxo45@uc2n273.localdomain>
2024-03-20 12:31:58 +0100

    Adding enroot.

---
+++ scripts/enroot-import.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ENROOT_SQUASH_OPTIONS='-comp lz4 -noD' enroot import docker://aakarsh/llm_calibration:latest
---
+++ scripts/enroot-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+enroot start  -w aakarsh+llm_calibration+latest.sqsh
