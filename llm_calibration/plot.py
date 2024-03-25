dac182ee587603d3bdf4ff80b56ac654386d0fdc
aakarsh nair <aakarsh@nentei.com>
2024-03-25 23:37:28 +0100

    Adding in plots of 7b vs 13b and 13b vs 13b

--- llm_calibration/plot.py
+++ llm_calibration/plot.py
@@ -13,15 +13,21 @@ import matplotlib.pyplot as plt
 # and the labels, , then you normalized frequency of the labels 1 for the y coordinate. 
 #   
 # A -(.9,0), B - 1 , C - 0  D 0 
-def plot_calibration(prediction_probabilities, actual_labels,num_bins=50, range_start = 0 , range_end=.00001, out_file=None):
+def plot_calibration(prediction_probabilities, actual_labels,
+                     num_bins=10, range_start = 0, range_end=1, 
+                     model_label="Model Calibration",
+                     out_file=None, figure=None, show_figure=False):
   """
-  Plot the calibration curves for the predicted probabilities of a model.
+    Plot the calibration curves for the predicted probabilities of a model.
   """
   sns.set_theme()
+
   # Sort predictions and corresponding actual labels
   sorted_indices = np.argsort(prediction_probabilities)
-  sorted_probs = prediction_probabilities[sorted_indices]
-  sorted_labels = actual_labels[sorted_indices]
+  prediction_probabilities = np.array(prediction_probabilities)
+  actual_labels = np.array(actual_labels)
+  sorted_probs = prediction_probabilities[sorted_indices.astype(int)]
+  sorted_labels = actual_labels[sorted_indices.astype(int)]
 
   # Create equal-sized bins
   bin_edges = np.linspace(range_start, range_end, num_bins)
@@ -32,7 +38,8 @@ def plot_calibration(prediction_probabilities, actual_labels,num_bins=50, range_
   for i in range(len(sorted_probs)):
       bin_index = np.digitize(sorted_probs[i], bin_edges) - 1
       if len(bin_counts) < bin_index:
-          print("bin_index", bin_index)
+          pass
+          #print("bin_index", bin_index)
       bin_counts[bin_index] += 1
       bin_correct[bin_index] += sorted_labels[i]
       
@@ -41,21 +48,56 @@ def plot_calibration(prediction_probabilities, actual_labels,num_bins=50, range_
   zero_counts_idx = np.where(bin_counts == 0)[0]
 
   # Set accuracy to 0 for bins with zero counts
-  bin_accuracy[zero_counts_idx] = 0
+  bin_accuracy[zero_counts_idx] = np.nan
 
   # Calculate accuracy for bins with non-zero counts
   bin_accuracy[bin_counts != 0] = bin_correct[bin_counts != 0] / bin_counts[bin_counts != 0]
   bin_means = (bin_edges[:-1] + bin_edges[1:]) / 2  # For plotting midpoints
 
+  mask = np.isfinite(bin_accuracy)
+  mask = mask[:-1] # Last bin is not included in mask
   # Create the calibration chart
+  if not figure: 
+    figure = plt.figure(figsize=(10, 7))
+    
   plt.plot(bin_means, bin_means, '--', color='gray', label='Perfect Calibration')  # Diagonal line
-  plt.plot(bin_means, bin_accuracy[:-1], 'o-', label='Model Calibration')
+  plt.plot(bin_means[mask], bin_accuracy[:-1][mask], 'o-', label=model_label)
   plt.xlabel('Prediction Probability (P(True))')
   plt.ylabel('Frequency of Correct Predictions')
   plt.title('Calibration Chart')
   plt.legend()
-  if out_file: plt.savefig(out_file)
-  plt.show()
-    
+
+  if out_file: 
+    plt.savefig(out_file)
+  if show_figure: 
+    plt.show()
+  
+  return figure
+
+#%%
+def plot_calibration_comparison(model_tags, model_labels, 
+                                prediction_probabilities, 
+                                actual_labels, 
+                                num_bins=10, range_start = 0 , range_end=1, out_file=None, show_figure=False):
+  """
+  Plot the calibration comparison plot.
+  """
+  save_fig = plt.figure(figsize=(10, 7))
+  for idx, model_key in enumerate(model_tags):
+    probabilities = prediction_probabilities[model_key]
+    truth_values = actual_labels[model_key] 
+    assert len(probabilities) == len(truth_values)
+    plot_calibration(probabilities, truth_values, 
+                     num_bins, range_start, range_end,
+                     model_label=model_labels[idx], 
+                     figure=save_fig, 
+                     show_figure=False) 
+  if out_file: 
+    save_fig.savefig(out_file)
+  if show_figure: 
+    save_fig.show()
+  return save_fig
 # Plot the calibration effect of the following
 # convert boolean array to np.int32
+
+# %%
--- scripts/generate-report-plots.py
+++ scripts/generate-report-plots.py
@@ -13,8 +13,12 @@ report_path = os.path.abspath(os.path.abspath(os.path.dirname(__file__))+"/../re
  
 sys.path.append(os.path.abspath(file_path+'/..'))
 #%%
+import importlib
+import llm_calibration
+importlib.reload(llm_calibration)
 from llm_calibration.model.model_probability import get_normalized_probabilities
 from llm_calibration.plot import plot_calibration
+from llm_calibration.plot import plot_calibration_comparison
 
 #%% 
 spyder_mode='IPYKERNEL_CELL_NAME' in os.environ
@@ -30,28 +34,80 @@ def generate_calibration_plot(file_path, output_dir=None, output_tag=None):
   plot_calibration(np.array(completion_probabilities), 
                   np.array(truth_values, dtype=np.int32), 
                   num_bins=10, range_start=0, range_end=1, out_file=output_dir+"/"+output_tag+".png")
- 
-def generate_comparison_plot(file_paths,  model_tags, output_dir, output_tag=None):
+
+#%%  
+def generate_comparison_plot(file_paths,  model_labels=[], 
+                             output_dir=None, output_tag=None):
+    """
+    Generate a comparison on multiple runs of a model.
+    """
     comparison_files = []
-    with open(file_path) as f:
-        comparison_files.append(json.load(f))
+   
+    for file_path in file_paths: 
+        with open(file_path) as f:
+            comparison_files.append(json.load(f))
         
-    model_results = [] 
     model_completion_probabilities={}
     model_truth_values = {}
-    for idx, comparison_file in enumerate(comparison_files):
-        model_results = comparison_file[0]['results']
-    completion_probabilities, truth_values = get_normalized_probabilities(model_results)
-    assert len(completion_probabilities) == len(truth_values)
- 
-    pass
     
+    for idx, comparison_file in enumerate(comparison_files):
+        model_results = comparison_file 
+        completion_probabilities, truth_values = \
+            get_normalized_probabilities(model_results)
+        current_label: str  = model_labels[idx]
+        model_completion_probabilities[current_label] = \
+            completion_probabilities
+        model_truth_values[current_label] = truth_values
+       
+    assert len(completion_probabilities) == len(truth_values)
+    
+    # model_tags, 
+    # model_labels, 
+    # prediction_probabilities, 
+    # actual_labels, num_bins=10, 
+    # range_start = 0 , 
+    # range_end=1, 
+    # out_file=None, 
+    # show_figure=False
+    plot_calibration_comparison(model_labels, model_labels, 
+                                model_completion_probabilities,
+                                model_truth_values,
+                                range_start=0, range_end=1, 
+                                out_file=output_dir+"/"+output_tag+".png")
+                                
+#%%   
 # Generate the 0-shot calibration plot between 7-b, 14-b and 70-b models
-generate_calibration_plot(os.path.abspath(file_path+'/../test/data/test_data.json'), output_dir=report_path, output_tag="0-shot")
+# model_results_model_meta-llama_Llama-2-13b-hf_ds_all_tag-result.json
+model_result_files = [
+   os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-13b-hf_ds_all_tag-result.json'), 
+   os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-13b-chat-hf_ds_all_tag-result.json') 
+]
+generate_comparison_plot(model_result_files, 
+                         ["Llama 13-b Base Model", 
+                          "Llama 13-b Chat Model"
+                          ], 
+                         output_dir=report_path, 
+                         output_tag="0-shot-13-b-chat-vs-13-b-hf")
+
+#%%
+#%%   
+# Generate the 0-shot calibration plot between 7-b, 14-b and 70-b models
+# model_results_model_meta-llama_Llama-2-13b-hf_ds_all_tag-result.json
+model_result_files = [
+   os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-13b-chat-hf_ds_all_tag-result.json'), 
+   os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-7b-chat-hf_ds_all_tag-result.json') 
+]
+generate_comparison_plot(model_result_files, 
+                         ["Llama 13b Chat Model", 
+                          "Llama 7b Chat Model"
+                          ], 
+                         output_dir=report_path, 
+                         output_tag="0-shot-7b-vs-13b-chat")
+
 
 # Generate the 5-shot calibration plot between 7-b, 14-b and 70-b models 
-generate_calibration_plot(os.path.abspath(file_path+'/../test/data/test_data.json'), output_dir=report_path, output_tag="5-shot")
+# generate_calibration_plot(os.path.abspath(file_path+'/../test/data/test_data.json'), output_dir=report_path, output_tag="5-shot")
 
 # Generate Subject-wise calibration plots between 7-b, 14-b models.
-generate_calibration_plot(os.path.abspath(file_path+'/../test/data/test_data.json'), output_dir=report_path, output_tag="by_subject")
-# %%
+# generate_calibration_plot(os.path.abspath(file_path+'/../test/data/test_data.json'), output_dir=report_path, output_tag="by_subject")
+#%%
