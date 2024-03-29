1e68e0dc5d2a81cd537dbfe858c257b363f9b1ba
aakarsh nair <aakarsh@nentei.com>
2024-03-29 16:51:10 +0100

    adding logic_qa

---
+++ llm_calibration/runner/logic_qa.py
@@ -0,0 +1,33 @@
+import datasets as hugging_face_datasets
+import numpy as np
+
+import llm_calibration.runner.multiple_choice_questions as mcq
+from llm_calibration.runner.multiple_choice_questions import  (run_inference) 
+                                
+def load_dataset(split="test"):
+    return hugging_face_datasets.load_dataset('lucamccabe/logiqa', split)
+
+def parse_dataset_item(item):
+    context = item["context"]
+    query = item["query"]
+    options = item["options"]
+    
+    question = f"{context}\nQuestion:{query}".format(context=context, query=query)
+    return {
+        "question": question,
+        "choices": options,
+        "answer": item["correct_option"]
+    }
+    
+def run_inference(model, tokenizer, dataset,
+                  tag="default_tag", include_prompt=False, 
+                  alphanumeric_options = ['A', 'B', 'C', 'D'],
+                  verbose = False, 
+                  n_shots=1):
+    return mcq.run_inference(model, tokenizer, dataset,
+                         tag=tag, 
+                         include_prompt=include_prompt, 
+                         dataset_item_parser=parse_dataset_item,
+                         alphanumeric_options=alphanumeric_options,
+                         verbose=verbose, 
+                         n_shots=n_shots)
--- llm_calibration/runner/multiple_choice_questions.py
+++ llm_calibration/runner/multiple_choice_questions.py
@@ -3,102 +3,19 @@ import numpy as np
 
 from llm_calibration.model.model_probability import  get_log_prob_of_completion
 
-STEM_DATASETS = [
-      'abstract_algebra',
-      'anatomy',
-      'astronomy',
-      'college_biology',
-      'college_chemistry',
-      'college_computer_science',
-      'college_mathematics',
-      'college_physics',
-      'computer_security',
-      'conceptual_physics',
-      'electrical_engineering',
-      'elementary_mathematics',
-      'high_school_biology',
-      'high_school_chemistry',
-      'high_school_computer_science',
-      'high_school_mathematics',
-      'high_school_physics',
-      'high_school_statistics',
-      'machine_learning',
-]
- 
-HUMANITIES_DATASET = [
-   'formal_logic',
-   'high_school_european_history',
-   'high_school_us_history',
-   'high_school_world_history',
-   'human_sexuality',
-   'international_law',
-   'jurisprudence',
-   'logical_fallacies',
-   'moral_disputes',
-   'moral_scenarios',
-   'philosophy',
-   'prehistory',
-   'professional_law',
-   'world_religions'
-]
-
-SOCIAL_SCIENCE_DATASET = [
-  'econometrics',
-  'high_school_geography',
-  'high_school_government_and_politics',
-  'high_school_macroeconomics',
-  'high_school_microeconomics',
-  'high_school_psychology',
-  'human_sexuality',
-  'professional_psychology',
-  'public_relations',
-  'security_studies',
-  'sociology',
-  'us_foreign_policy'
-]
-
-OTHER_DATASET = [
-  'business_ethics',
-  'clinical_knowledge',
-  'college_medicine',
-  'global_facts',
-  'human_aging',
-  'management',
-  'marketing',
-  'medical_genetics',
-  'miscellaneous',
-  'nutrition',
-  'professional_accounting',
-  'professional_medicine',
-  'virology',
-]
-
-def load_dataset(name, split="test"):
-  datasets = []
-  if name == "STEM":
-          datasets = [hugging_face_datasets.load_dataset("cais/mmlu", dataset )[split] for dataset in STEM_DATASETS]
-  elif name == "HUMANITIES":
-          datasets = [hugging_face_datasets.load_dataset("cais/mmlu", dataset)[split] for dataset in HUMANITIES_DATASET]
-  elif name == "SOCIAL_SCIENCE":
-          datasets = [hugging_face_datasets.load_dataset("cais/mmlu", dataset)[split] for dataset in SOCIAL_SCIENCE_DATASET]
-  elif name == "OTHER":
-          datasets = [hugging_face_datasets.load_dataset("cais/mmlu", dataset)[split] for dataset in OTHER_DATASET]
-  else: # built in dataset
-    return hugging_face_datasets.load_dataset("cais/mmlu", name)[split]
-  return hugging_face_datasets.concatenate_datasets(datasets, split=split)
-
-
 def generate_n_shot_prompt(dataset,
                            question_idx,
                            prompt_template="{question}",
                            options_template="({choice})",
                            answer_template="Answer:{answer}",
+                           item_parser=lambda x: x,
+                           alphanumeric_options = ['A', 'B', 'C', 'D'],
                            n_shots=5):
   """
   Generate a n-shot prompt.
   """
   def format_question(current_idx, with_answer=True):
-    item = dataset[current_idx]
+    item = item_parser(dataset[current_idx]) 
     question_template = prompt_template.format(question=item["question"])
     choices_template = "\n"+"\n".join(["%s. %s" % (options_template.format(choice=alpha), choice) for alpha, choice in zip(alphanumeric_options, item['choices'])])
     question_prompt = "%s\n%s\n" % (question_template, choices_template)
@@ -108,7 +25,6 @@ def generate_n_shot_prompt(dataset,
       question_prompt += "\n"+ answer_template.format(answer="")
     return question_prompt
  
-  alphanumeric_options = ['A', 'B', 'C', 'D']
   formatted_options = ["(%s)" % choice for choice in alphanumeric_options ]
   current_idx = 0 
   question_buffer = ""
@@ -123,8 +39,11 @@ def generate_n_shot_prompt(dataset,
   question_buffer += format_question(question_idx, with_answer=False)
   return question_buffer, formatted_options
 
+
 def run_inference(model, tokenizer, dataset,
                   tag="default_tag", include_prompt=False, 
+                  dataset_item_parser = lambda x: x,
+                  alphanumeric_options = ['A', 'B', 'C', 'D'],
                   verbose = False, 
                   n_shots=1):
   results = []
@@ -132,8 +51,8 @@ def run_inference(model, tokenizer, dataset,
   target_labels = []
 
   for question_idx, item  in enumerate(dataset):
-    alphanumeric_options = ['A', 'B', 'C', 'D'] 
-    prompt, selections = generate_n_shot_prompt(dataset, question_idx, n_shots=n_shots)
+    item = dataset_item_parser(item)
+    prompt, selections = generate_n_shot_prompt(dataset, question_idx, n_shots=n_shots, item_parser=dataset_item_parser)
     if verbose: 
       print(prompt)
     selection_log_prob_opt_option = []
@@ -169,4 +88,4 @@ def run_inference(model, tokenizer, dataset,
       result["prompt_template"] = prompt 
     results.append(result)
 
-  return results, prediction_probabilities, target_labels
+    return results, prediction_probabilities, target_labels 
