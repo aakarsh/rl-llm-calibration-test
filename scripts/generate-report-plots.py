54d068e44bcddf6d5e4f1e2132ce5fd6eaec4056
aakarsh nair <aakarsh@nentei.com>
2024-03-30 12:10:58 +0100

    adding reporting plots.

--- scripts/generate-report-plots.py
+++ scripts/generate-report-plots.py
@@ -95,13 +95,17 @@ generate_comparison_plot(model_result_files,
 #%% 5-shot logic-qa 
 model_result_files = [
     os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-7b-chat-hf_ds_lucasmccabe_logiqa_n_shots_5_tag-result.json'),
+    os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-7b-hf_ds_lucasmccabe_logiqa_n_shots_5_tag-result.json'),
     os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-13b-chat-hf_ds_lucasmccabe_logiqa_n_shots_5_tag-result.json') 
 ]
 generate_comparison_plot(model_result_files, 
-                          ['Llama 7b Chat Model (5-shot)', 'Llama 13b Chat Model (5-shot)'], 
+                          ['Llama 7b Chat Model (5-shot)', 
+                           'Llama 7b Base Model (5-shot)', 
+                           'Llama 13b Chat Model (5-shot)'], 
                          output_dir=report_path, 
                          output_tag="5-shot-7b-13b-logic-qa")
-#%% hrunning on human eval
+
+#%% Running on human eval
 model_result_files = [
     os.path.abspath(file_path+'/../output/model-output/model_results_model_meta-llama_Llama-2-7b-chat-hf_ds_openai_humaneval_n_shots_5_tag-result.json')
 ]
