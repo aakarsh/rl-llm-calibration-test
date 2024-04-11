f54830a2993cad3ea79acaef157764821e66c842
aakarsh nair <aakarsh@nentei.com>
2024-04-11 15:13:24 +0200

    minor support for logic_qa

--- llm_calibration/runner/logic_qa.py
+++ llm_calibration/runner/logic_qa.py
@@ -4,8 +4,8 @@ import numpy as np
 import llm_calibration.runner.multiple_choice_questions as mcq
 from llm_calibration.runner.multiple_choice_questions import  (run_inference) 
                                 
-def load_dataset(name="lucamccabe/logiqa", split='train'):
-    return hugging_face_datasets.load_dataset(name)[split]
+def load_dataset(name=None, split='train'):
+    return hugging_face_datasets.load_dataset("lucamccabe/logiqa")[split]
 
 def parse_dataset_item(item):
     context = item["context"]
