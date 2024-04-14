2a4ba457de0287ee82a5e28c221fdd2c6b04dc2d
aakarsh nair <aakarsh@nentei.com>
2024-04-14 08:55:58 +0200

    adding nshots=1.

--- scripts/generate-report-plots.py
+++ scripts/generate-report-plots.py
@@ -85,19 +85,22 @@ generate_roc_plot(model_result_files, [
 #%% MMLU-Subject Specific 0-shot 
 ## TODO: 7b , 7b-chat, 13b
 subjects = ["STEM", "SOCIAL_SCIENCE", "HUMANITIES", "OTHER"]
-model_result_files = [ os.path.abspath(file_path+('/../output/model-output/model_results_model_meta-llama_Llama-2-13b-chat-hf_ds_%s_tag-result.json'% subject)) for subject in subjects]
-generate_comparison_plot(model_result_files, 
-                          ["Llama 13-b Chat Model %s (0-shot)" % subject.upper() for subject in subjects], 
+model_result_files =  \
+   [ os.path.abspath(file_path+('/../output/model-output/model_results_model_meta-llama_Llama-2-13b-chat-hf_ds_%s_tag-result.json'% subject)) for subject in subjects] + \
+   [ os.path.abspath(file_path+('/../output/model-output/model_results_model_meta-llama_Llama-2-7b-chat-hf_ds_%s_n_shots_1_tag-result.json'% subject)) for subject in subjects] 
+
+model_labels = ["Llama 13-b Chat Model %s (0-shot)" % subject.upper() for subject in subjects] + \
+               ["Llama 7-b Chat Model %s (0-shot)" % subject.upper() for subject in subjects] 
+
+generate_comparison_plot(model_result_files, model_labels, 
                          dynamic_bins=True, 
                          samples_per_bin=300,
                          output_dir=report_path, 
                          output_tag="0-shot-MMLU-subjects")
 
-generate_roc_plot(model_result_files, 
-                          ["Llama 13-b Chat Model %s (0-shot)" % subject.upper() for subject in subjects], 
+generate_roc_plot(model_result_files, model_labels, 
                          output_dir=report_path, 
                          output_tag="0-shot-MMLU-subjects")
-
 
 #%% MMLU-Subject Specific 5-shot
 ## TODO: 7b , 7b-chat, 13b
